b32425ab1776f7b3ef6ea2eaff1688cf8198f901
denLillaFlickan <pviktorialilla@gmail.com>
2023-08-18 12:37:44 +0200

    working on kezdolap, flask and jinja

---
+++ app.py
@@ -0,0 +1,23 @@
+import os
+
+from flask import Flask, flash, redirect, render_template, request, session
+from tempfile import mkdtemp
+from re import search
+
+# Configure application
+app = Flask(__name__)
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+@app.kezdolap("/")
+def kezdolap():
+    """Betölti a kezdőlapot"""
+    if request.method == "GET":
+        return render_template("kezdolap.html")
