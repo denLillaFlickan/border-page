8caae5735e72fe4718651a2a028b09c827f8abac
Lilla <root@LAPTOP-FCMSP0E1>
2023-08-18 15:06:34 +0200

    Program is running, working on logo

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import os
 
+from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
 from tempfile import mkdtemp
-from re import search
 
 # Configure application
 app = Flask(__name__)
